1d806d833c3209d0a74a85c3bf5367683df93166
esraaKd <esraakdm13@gmail.com>
2024-07-21 23:46:10 +0300

    form and table

---
+++ dom/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form'); 
+    const table = document.getElementById('table'); 
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+       
+        const formData = new FormData(form);
+        const formProps = Object.fromEntries(formData);
+
+    
+        const newRow = table.insertRow();
+
+        
+        Object.values(formProps).forEach(value => {
+            const newCell = newRow.insertCell();
+            newCell.textContent = value;
+        });
+
+
+    });
+});
